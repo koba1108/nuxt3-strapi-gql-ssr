3e70544933084420ff949518273115a611a7714b
koba1108 <paseli.koba@gmail.com>
2023-02-12 15:04:40 +0900

    add

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nuxt3-strapi-gql-ssr
 nuxt3-strapi-gql-ssr
+
+
+# tools alloy-db-auth-proxy
+https://cloud.google.com/alloydb/docs/auth-proxy/connect
--- strapi/config/database.js
+++ strapi/config/database.js
@@ -1,17 +1,17 @@
-// const path = require('path');
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: env('DATABASE_CLIENT', 'mysql'),
+module.exports = ({ env }) => {
+  return {
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
-      ssl: env.bool("DATABASE_SSL", false),
+      client: env('DATABASE_CLIENT', 'mysql'),
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      useNullAsDefault: true,
+      debug: env.bool("DATABASE_DEBUG", true),
     },
-    useNullAsDefault: true,
-    // debug: env.bool("DATABASE_DEBUG", true),
-  },
-});
+  }
+}
